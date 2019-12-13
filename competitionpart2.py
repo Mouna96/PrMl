e382e0859e483d5e2d712c198e70d54c69fbd609
Mouna96 <49752663+Mouna96@users.noreply.github.com>
2019-12-13 12:44:08 +0200

    Add files via upload

--- competitionpart2.py
+++ competitionpart2.py
@@ -7,8 +7,11 @@ import numpy as np
 import matplotlib.pyplot as plt
 import os
 import sys 
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+
 
 import cv2
+from sklearn.utils import class_weight
 
 from sklearn.model_selection import train_test_split
 from sklearn.model_selection import cross_val_score
@@ -86,9 +89,37 @@ Y=np.load('yimg_data.npy')
 
 
 X_train, X_tst, y_train, y_test = train_test_split(X, Y, test_size = 0.2)
+####
+
+# CALCULATE THE WEIGHTS 
+
+class_weights = class_weight.compute_class_weight('balanced',
+                                                 np.unique(y_train),
+                                                 y_train)
+class_weights = dict(enumerate(class_weights))
 
 y_train = tensorflow.keras.utils.to_categorical(y_train, num_classes)
 y_test = tensorflow.keras.utils.to_categorical(y_test, num_classes)
+
+datagen = ImageDataGenerator(
+    featurewise_center=True,
+    featurewise_std_normalization=True,
+    rotation_range=20,
+    width_shift_range=0.2,
+    height_shift_range=0.2,
+    rescale=1./255,
+    shear_range=0.2,
+    zoom_range=0.2,
+    horizontal_flip=True,
+    fill_mode='nearest')
+# compute quantities required for featurewise normalization
+# (std, mean, and principal components if ZCA whitening is applied)
+datagen.fit(X_train)
+# fits the model on batches with real-time data augmentation:
+
+
+
+
 # Classifiers
 
 #base_model = tensorflow.keras.applications.mobilenet.MobileNet(input_shape=(224,224,3),include_top = False, alpha=0.25)
@@ -97,6 +128,8 @@ y_test = tensorflow.keras.utils.to_categorical(y_test, num_classes)
 #                                                                    weights='imagenet', input_tensor=None, pooling=None, classes=17)
 base_model = tensorflow.keras.applications.inception_v3.InceptionV3(include_top=False, weights='imagenet',
                                                                     input_tensor=None, input_shape=(224,224,3), pooling=None, classes=17)
+
+
 
 
 in_tensor = base_model.inputs[0] # Grab the input of base model
@@ -109,13 +142,17 @@ model = tensorflow.keras.models.Model(inputs = [in_tensor],outputs = [out_tensor
 model.summary()
 batch_size = 50
 
-epochs = 15
+epochs = 30
 
 model.compile(loss=tensorflow.keras.losses.categorical_crossentropy,
               optimizer=tensorflow.keras.optimizers.Adadelta(),
               metrics=['accuracy'])
 
-model.fit(X_train, y_train,batch_size=batch_size,epochs=epochs,verbose=1,validation_data=(X_tst, y_test))
+model.fit_generator(datagen.flow(X_train, y_train, batch_size=128),
+                    steps_per_epoch=len(X_train) /128, epochs=15)
+
+model.fit(X_train, y_train,batch_size=batch_size,epochs=epochs,
+           class_weight=class_weights,verbose=1,validation_data=(X_tst, y_test))
 score = model.evaluate(X_tst, y_test, verbose=0)
 
 print('Test loss:', score[0])
@@ -123,37 +160,32 @@ print('Test loss:', score[0])
 print('Test accuracy:', score[1])
 
 
+from pathlib import Path
 
 
 
-model=load_model('incep_86.h5')
+model=load_model('incep_trial_weights.h5')
 
 
-
+basepath = Path("C:/Work/sgndataset/testset/")
+files_in_basepath = basepath.iterdir()
 with open("submission.csv", "w") as fp:
-    fp.write("Id, Category\n")
-    for image in os.walk(r"C:/Work/sgndataset/testset/"):
+    fp.write("Id,Category\n")
+    for image  in files_in_basepath:
+#        print(image)
+        img = plt.imread(image)
+        image=str(image).split('\\')[-1]
         image=image.split('.')[0]
-# 1. load image and resize
-        img = cv2.resize(image, (224, 224))
-            
-            # Convert the data to float, and remove mean:
+        img = cv2.resize(img, (224, 224))
+#
         img = img.astype(np.float32)
         img -= 128
-            
-            # Push the data through the model:
-#            x = model.predict(img[np.newaxis, ...])[0]
-            
-            # And append the feature vector to our list.
-        pred=model.predict(img)
-            
+#
+        pred=model.predict(img[np.newaxis, ...])[0]
+        index = np.argmax(pred, axis=0)
             # Extract class name from the directory name:
-        label = label = class_names[pred]
-            
-# 2. vectorize using the net
-# 3. predict class using the sklearn model
-# 4. convert class id to name (label = class_names[class_index])
-fp.write("%d,%s\n" % (image, label))
+        label = class_names[index]
+        fp.write("%d,%s\n" % (int(image), label))
 
 
 
